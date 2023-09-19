d8abf8b1c8d871338d585dbe88304547bb423dbb
ifedayoprince@gmail.com <ifedayoprince@gmail.com>
2023-09-19 13:40:31 +0100

    Jj

--- server/socket.js
+++ server/socket.js
@@ -4,7 +4,14 @@ import { Server, Socket } from 'socket.io';
 
 // Initialize Socket.io logic
 export function initializeSocketIO(server) {
-  const io = new Server(server);
+  const io = new Server(server, {
+  	cors: {
+  		origin: (_req, callback) => {
+  			callback(null, true);
+  		},
+  		credentials: true
+  	}
+  });
 
   io.on('connection', (socket) => {
     console.log('A user connected');
