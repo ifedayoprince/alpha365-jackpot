f4271e11c7ee68ee0b15c79e73eb4ded600f1c90
ifedayoprince@gmail.com <ifedayoprince@gmail.com>
2023-09-19 11:55:50 +0100

    Srr

--- client/src/pages/Page.tsx
+++ client/src/pages/Page.tsx
@@ -10,12 +10,8 @@ import Gameover from '../components/Gameover';
 import BombMode from '../components/Bomb';
 import Menu from '../components/Menu';
 
-import {socketConnect} from './../models/Socket';
-
 const Page = () => {
-	useEffect(()=>{
-		socketConnect();
-	}, [])
+	
 	return (<section className="page minor-jackpot">
 
 		<Navbar />
